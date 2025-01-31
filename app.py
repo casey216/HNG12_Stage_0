3ad09b410c39695bcfda00bc46ac92517f0f3de9
Kenechi <caseynzewi@gmail.com>
2025-01-31 13:12:24 +0100

    Updated app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ CORS(app)
 
 @app.route("/")
 def index():
-    current_date = datetime.now().isoformat()
+    current_date = datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')
     info = {
         "email": "caseynzewi@gmail.com",
         "current_datetime": current_date,
