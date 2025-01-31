e1905399effc02212f62676510567d3666e3456a
Kenechi <caseynzewi@gmail.com>
2025-01-31 09:56:45 +0100

    implemented API

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
+
+from datetime import datetime
 
 app = Flask(__name__)
+app.json.sort_keys = False
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    current_date = datetime.now().isoformat()
+    info = {
+        "email": "caseynzewi@gmail.com",
+        "current_datetime": current_date,
+        "github_url": "<https://github.com/casey216/HNG12_Stage_0>"
+    }
+    return jsonify(info), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
